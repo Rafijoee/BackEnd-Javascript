61576f242f9ec1b02ef0608564e86ec36e09e29a
Rafijoee <rafijoe44@gmail.com>
2024-10-01 20:19:36 +0700

    feat : setup express js

--- first-express/index.js
+++ first-express/index.js
@@ -1,2 +1,8 @@
 const express = require('express');
+const app = express(); 
 
+
+app.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080');
+    }
+); // untuk menjalankan server
