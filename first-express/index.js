efee93fdb04c978f72c523425190909d6f24b4aa
Rafijoee <rafijoe44@gmail.com>
2024-10-04 21:51:09 +0700

    feat : routes in express

--- first-express/index.js
+++ first-express/index.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express(); 
 
 
+app.get('/about', (req, res) => {
+    res.send({ message: 'Ini adalah halaman about' });
+}); // ini router di express
+
+
+app.get('/blog/:category/:author/:title', (req, res) => {
+    const { category, author, title } = req.params;
+    res.send(`Category: " + ${category} + ", Author: " + ${author} + ", Title: " + ${title}`);
+}); // ini router di express dengan parameter
+
+app.get('*', (req, res) => {
+    res.send({ message: 'Halaman tidak ditemukan' });
+ }); // ini router di express untuk halaman yang tidak ditemukan (ditaruk di paling bawah)
+
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
-    }
-); // untuk menjalankan server
+}); // untuk menjalankan server
